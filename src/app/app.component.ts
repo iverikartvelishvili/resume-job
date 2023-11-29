4b5e139268fa70b91d997a5e4bdf8b1028b013cb
iveri-kartvelishvili <iveri.kartvelishvili2017@gmail.com>
2023-11-30 00:10:28 +0400

    addresume

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,46 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'resume';
+  jobForm!:FormGroup
+
+  constructor(private fBuilder:FormBuilder){
+     this.jobForm =  this.fBuilder.group({
+     jobs:this.fBuilder.array([])
+    })
+  }
+  getJobs():FormArray{
+    return this.jobForm.get('jobs') as FormArray
+  }
+  newJob():FormGroup{
+    return this.fBuilder.group({
+      companyName:"",
+      companyWorkDescription:"",
+      workExps:this.fBuilder.array([])
+    })
+  }
+  getWorkExps(jobIndex:number):FormArray{
+     return this.getJobs().at(jobIndex).get("workExps") as FormArray
+  }
+  newWorkExp():FormGroup{
+    return this.fBuilder.group({
+      position:"",
+      startAt:"",
+      endAt:""
+    })
+  }
+  addNewJob(){
+    this.getJobs().push(this.newJob())
+  }
+  deleteJob(jobIndex:number){
+    this.getJobs().removeAt(jobIndex)
+  }
+  addNewWorkExp(jobIndex:number){
+    this.getWorkExps(jobIndex).push(this.newWorkExp())
+  }
+  deleteJobExp(jobIndex:number, expIndex:number){
+    this.getWorkExps(jobIndex).removeAt(expIndex)
+  }
+  onFormSubmit(){
+    console.log(this.jobForm.value)
+  }
 }
